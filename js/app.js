ac087388db3c19cd4b905f76ebbf37dba36965b2
cindyhanae <cinde01@gmail.com>
2021-09-29 16:04:43 -0300

    header toggle background

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-$(body).scroll(function() {
-    alert('scroll')
+$('.body').scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('header').addClass('active_header')
+        $('.logo').html(
+            '<img src="./assets/logo-black.svg" alt="Logo C6 Bank">'
+        )
+    } else {
+        $('header').removeClass('active_header')
+        $('.logo').html(
+            '<img src="./assets/logo-white.svg" alt="Logo C6 Bank">'
+        )
+    }
 })
 
+$('.toggle').on('click', () =>{
+    $('.toggle')
+})
