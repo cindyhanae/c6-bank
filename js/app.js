d81ed18402943999df7e8156ebee04bedcd95ced
cindyhanae <cinde01@gmail.com>
2021-10-19 22:36:18 -0300

    menu fixed

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-$('.body').scroll(function(){
-    if($(this).scrollTop() > 100){
-        $('header').addClass('active_header')
-        $('.logo').html(
-            '<img src="./assets/logo-black.svg" alt="Logo C6 Bank">'
-        )
-    } else {
-        $('header').removeClass('active_header')
-        $('.logo').html(
-            '<img src="./assets/logo-white.svg" alt="Logo C6 Bank">'
-        )
-    }
-})
 
-$('.toggle').on('click', () =>{
-    $('.toggle')
-})
+
+const header = document.querySelector('header')
+const logoChange = document.querySelector('.js-header a img')
+
+function menuFixo(){
+    if (window.scrollY > 200) {
+        header.classList.add('active_header')
+        logoChange.setAttribute('src', './assets/logo-black.svg')
+
+    } else {
+        header.classList.remove('active_header')
+        logoChange.setAttribute('src', './assets/logo-white.svg')
+    }
+}
+
+window.addEventListener('scroll', menuFixo)
