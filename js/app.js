9bfbd56d58ece64705e1127f8c6b0fca2b740258
cindyhanae <cinde01@gmail.com>
2021-10-21 15:31:49 -0300

    fixed header update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
-
-
 const header = document.querySelector('header')
 const logoChange = document.querySelector('.js-header a img')
 
 function menuFixo(){
     if (window.scrollY > 200) {
         header.classList.add('active_header')
-        logoChange.setAttribute('src', './assets/logo-black.svg')
 
     } else {
         header.classList.remove('active_header')
-        logoChange.setAttribute('src', './assets/logo-white.svg')
     }
 }
 
